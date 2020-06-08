33826ea60bec1bf83198b357365b767702de54ba
Julien <julien@rbrt.fr>
2020-06-08 16:10:44 +0200

    add small generate user test

--- generate_test.go
+++ generate_test.go
@@ -1,7 +1,39 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v5"
+)
 
 func TestGenerateUsers(t *testing.T) {
+	//set seed
+	gofakeit.Seed(42)
 
+	recipes := LoadCSV(recipesCSVPath)
+	users, err := generateUsers(15, recipes)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(users.Users) != 15 {
+		t.Errorf("Number of users is incorrect, got '%d', want '%d'", len(users.Users), 15)
+	}
+
+	for _, u := range users.Users {
+		//verify location
+		if u.Latitude > maxLatitudeNL {
+			t.Errorf("Latidude is incorrect, got '%v', shoud be maximum '%v'", u.Latitude, maxLatitudeNL)
+		}
+		if u.Latitude < minLatitudeNL {
+			t.Errorf("Latidude is incorrect, got '%v', shoud be minimum '%v'", u.Latitude, minLatitudeNL)
+
+		}
+		if u.Longitude > maxLongitudeNL {
+			t.Errorf("Latidude is incorrect, got '%v', shoud be maximum '%v'", u.Longitude, maxLongitudeNL)
+		}
+		if u.Longitude < minLongitudeNL {
+			t.Errorf("Latidude is incorrect, got '%v', shoud be minimum '%v'", u.Longitude, minLongitudeNL)
+		}
+	}
 }
